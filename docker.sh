20d94cedae740ca5476640d1376f36bf94cda60b
Yew, Chang Ching <chang.ching.yew@intel.com>
2024-10-12 06:42:40 +0800

    Nvidia tf docker requirements and script

---
+++ docker.sh
@@ -0,0 +1,1 @@
+docker run --gpus all -it -v /home/data/cyew3/gpu_auto/VIDMAP:/home/data/cyew3/gpu_auto/VIDMAP -v /home/data3/cyew3/VIDMAP:/home/data3/cyew3/VIDMAP tensorflow/tensorflow:1.15.2-gpu-py3
